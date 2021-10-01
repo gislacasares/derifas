e8d1dd26dd16d11e5a3aadcba6c2e954ff53a50c
Gaston Isla Casares <gislacasares@gmail.com>
2021-10-01 13:22:30 -0300

    cambio conf DB

--- database/config/config.js
+++ database/config/config.js
@@ -7,13 +7,13 @@ module.exports = {
         port: "3306",
         dialect: "mysql",
         /*
-            username: "derifas_user",
-                    password: "gato8objeto",
-                    database: "derifas",
-                    host: "localhost",
-                    port: "8889",
-                    dialect: "mysql",
-                */
+                username: "derifas_user",
+                        password: "gato8objeto",
+                        database: "derifas",
+                        host: "localhost",
+                        port: "8889",
+                        dialect: "mysql",
+                    */
     },
     test: {
         username: "root",
@@ -23,10 +23,11 @@ module.exports = {
         dialect: "mysql",
     },
     production: {
-        username: "root",
-        password: null,
-        database: "database_production",
-        host: "127.0.0.1",
+        username: "b73e483920df9a",
+        password: "dbf7401c",
+        database: "heroku_c901615016c2f69",
+        host: "us-cdbr-east-04.cleardb.com",
+        port: "3306",
         dialect: "mysql",
     },
 };
